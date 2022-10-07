b265d783fa7a80c6f24d7b8695f53e8508464d70
Trevor Crystal <TGCrystal@users.noreply.github.com>
2022-10-07 12:46:21 -0400

    Update ci.config.ts

--- playwright/config/ci.config.ts
+++ playwright/config/ci.config.ts
@@ -101,8 +101,7 @@ function getSnapshotConfig(command: string): PlaywrightTestConfig {
 let overrides: PlaywrightTestConfig = {};
 if (process.env.UPDATE_SNAPSHOTS_COMMAND) {
     overrides = getSnapshotConfig(process.env.UPDATE_SNAPSHOTS_COMMAND);
-    console.log("ENV TEST: " + process.env.UPDATE_SNAPSHOTS_COMMAND);
-    console.log("PLATFORM TEST: " + process.platform);
+    throw new Error("ENV TEST: " + process.env.UPDATE_SNAPSHOTS_COMMAND + "\nPLATFORM TEST: " + process.platform);
     // If there are no tests to run, then exit safely.
     // Playwright normally exists with status code 1 if no tests are run.
     if (Object.keys(overrides).length === 0)
